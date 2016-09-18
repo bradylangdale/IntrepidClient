ee9cd9f0ef5b3bb9ceb05feac265420990608e14
bradylangdale <bradylangdale@gmail.com>
2016-09-18 12:38:01 -0500

    Non-functional Files

--- Client Files/Launcher.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-title IntrepidClient Launcher
-
-start "" /b "./IntrepidClient/Hacks.lnk"
-
-start "" /b "./IntrepidClient-menu/Menu.lnk"
--- Client Files/Rebuild-build.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-title IntrepidClient Builder
-
-start "" /b "./IntrepidClient/BuildHacks.lnk"
-
-start "" /b "./IntrepidClient-menu/BuildMenu.lnk"
