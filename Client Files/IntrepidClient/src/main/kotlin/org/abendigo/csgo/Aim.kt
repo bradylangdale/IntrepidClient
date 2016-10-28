6b518190f1b5f6f1fbfaecdccb0e53fa6564d1e7
Brady Langdale <bradylangdale@gmail.com>
2016-10-27 22:12:22 -0500

    Oops

--- Client Files/IntrepidClient/src/main/kotlin/org/abendigo/csgo/Aim.kt
+++ Client Files/IntrepidClient/src/main/kotlin/org/abendigo/csgo/Aim.kt
@@ -159,5 +159,5 @@ fun calculateAngle(player: Player, src: Vector, dst: Vector, angles: Vector): Ve
 }
 
 fun distance(p1: Vector, p2: Vector) : Float {
-	return (sqrt((((p1.x*p1.x)+(p2.x*p2.x))+((p1.y*p1.y)+(p2.y*p2.y))+((p1.z*p1.z)+(p2.z*p2.z))).toDouble())).toFloat()
+	return (sqrt((((p1.x*p1.x)-(p2.x*p2.x))+((p1.y*p1.y)-(p2.y*p2.y))+((p1.z*p1.z)-(p2.z*p2.z))).toDouble())).toFloat()
  }
